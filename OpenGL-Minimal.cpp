89f19a0fe4b50928c25113c1af8860a772153b1e
Landon Smith <landonasmith@gmail.com>
2017-03-25 00:04:28 -0600

    Updated compile script with (hopefully working) build for linux. Removed multisampling.

--- OpenGL-Minimal.cpp
+++ OpenGL-Minimal.cpp
@@ -24,7 +24,6 @@ int main(int argc, char const *argv[]) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_SAMPLES, 4);
 
 	GLFWwindow* window = glfwCreateWindow(700, 700, "Hello Triangle", NULL, NULL);
 
--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+# macOS: install using brew install glfw3 --static
 clang++ -Wall -std=c++14 -I/usr/local/include -L/usr/local/lib -lglfw3 -framework OpenGL -framework CoreVideo -framework IOKit -framework Cocoa OpenGL-Minimal.cpp
+# ubuntu: install using sudo apt-get install libglfw3 libglfw3-dev
+# g++ -Wall -std=c++14 -lGL -lGLFW OpenGL-Minimal.cpp
